42c45189f3fd8dce9445100c96793990e0dff99c
Ryota Maeda <61216064+Ryot21@users.noreply.github.com>
2024-04-18 01:05:49 +0900

    エラーページ設定

--- Ryot21-Portfolio/pages/works/products/[id].vue
+++ Ryot21-Portfolio/pages/works/products/[id].vue
@@ -4,3 +4,11 @@
 		<h1>Product/{{ $route.params.id }}を表示中</h1>
 	</div>
 </template>
+
+<script setup>
+	definePageMeta({// 数字以外のパラメーターは「404エラー」を表示。
+		validate: async(route) => {
+			return /^\d+$/.test(route.params.id)
+		}
+	})
+</script>
