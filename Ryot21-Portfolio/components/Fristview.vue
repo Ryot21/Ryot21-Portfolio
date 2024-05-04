e095a3eaaad8353ab309a5871807c0af12a4efe0
Ryota Maeda <61216064+Ryot21@users.noreply.github.com>
2024-05-05 01:00:30 +0900

    ファーストビュー > デザイン修正

--- Ryot21-Portfolio/components/Fristview.vue
+++ Ryot21-Portfolio/components/Fristview.vue
@@ -4,8 +4,32 @@
 		<div class="imgBox -cv">
 			<!-- <img src="~assets/images/f-view/top/top-view_bg.png" alt="透明な背景"> -->
 		</div>
-		<div class="c-contentsBox -f-logo">
+		<div class="c-contentsBox -f-circle"><!-- ロゴ / サークル -->
+			<div class="">
+				<svg viewBox="0 0 100 100">
+					<path d="M 0,50 a 50,50 0 1,1 0,1 z" id="circle" />
+					<text>
+						<textPath xlink:href="#circle">
+							WEBENGiNEER&nbsp;&nbsp;&nbsp;&nbsp;HEML5CSS3CODER&nbsp;&nbsp;&nbsp;&nbsp;WEBDESIGNER&nbsp;&nbsp;&nbsp;&nbsp;SES&nbsp;&nbsp;&nbsp;&nbsp;
+						</textPath>
+					</text>
+				</svg>
+			</div>
+		</div>
+		<div class="c-contentsBox -f-logo"><!-- ロゴ / アイコン -->
 			<img src="~assets/images/logo/site-logo01.png" alt="">
 		</div>
+
 	</div>
 </template>
+
+<style lang="scss">
+.-f-circle{
+	svg {overflow: visible;}
+	path {fill: none;}
+	textPath{
+		font-size: .95rem;
+
+	}
+}
+</style>
