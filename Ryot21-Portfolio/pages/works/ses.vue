c860e9f89d2dee074199748a692ccd25eb4f3b60
Ryota Maeda <61216064+Ryot21@users.noreply.github.com>
2024-05-10 01:41:26 +0900

    SESページ > 完成

--- Ryot21-Portfolio/pages/works/ses.vue
+++ Ryot21-Portfolio/pages/works/ses.vue
@@ -1,7 +1,28 @@
 <!-- works > お仕事紹介ページ -->
 <template>
 	<div>
-		<PageView />
-		<h1>Work/Ses Page</h1>
+		<div class="c-contents mgt-contents">
+			<div class="c-contents__inner w1100">
+				<div class="c-profile mgb5 mgb10s">
+					<ruby class="s-S -ls-2 -left">エンジニア向けのお仕事紹介</ruby>
+					<h2 class="c-profile__title s-3L -b -ls-2 -left">System Engineering Service<span class="s-S">SES</span></h2>
+				</div>
+				<p class="c-profile__des s-M -left -lh-2 -ls-2 mgb10 mgb20s">
+					エンジニアの皆様へ<br>
+					<br>
+					経験の浅いエンジニア・コーダー志望の方や新しい技術や異なる言語のプロジェクトに挑戦したいと考えている方々をサポートするため、
+					エンジニア・コーダー向けにエージェント業も行っております。<br>
+					現状のプロジェクトに不満を抱えているエンジニアや
+					異なる技術に興味を持つエンジニアにとって、適切なプロジェクトを見つけることはキャリアの重要なステップです。
+					各エンジニアのスキルとキャリアの目標に合わせたプロジェクトを提供し、実務経験を積みながら自信と能力を高める機会を提供します。お気軽にお問い合わせください。
+				</p>
+
+				<!-- 今まで制作したWEBサイトのご紹介 -->
+				<div class="c-contents mgb20 mgb20s">
+
+				</div>
+			</div>
+		</div>
+
 	</div>
 </template>
