ac324f0f19e6cc3d2ec858d00963609bb7f05c93
Ryota Maeda <61216064+Ryot21@users.noreply.github.com>
2024-05-15 20:42:46 +0900

    パンくず_修正

--- Ryot21-Portfolio/components/parts/Breadcrumb.vue
+++ Ryot21-Portfolio/components/parts/Breadcrumb.vue
@@ -1,12 +1,24 @@
+<!-- パンくず -->
+<script setup lang="ts">
+
+	import type { Product } from "~~/types/Product";
+	const { params } = useRoute();
+	const { data } = await useMicroCMSGetListDetail<Product>({
+		endpoint: "products",
+		contentId: Array.isArray(params.id) ? params.id[0] : params.id,
+	});
+	console.log(data);
+
+</script>
 
 <template>
-	<ul class="c-breadcrumb__lists mgb5 mgb5s">
+	<ul class="c-breadcrumb__lists mgb2 mgb10s">
 			<li class="c-breadcrumb__item">
 					<NuxtLink class="c-breadcrumb__link" to="/">トップ</NuxtLink>
 			</li>
 			<li class="c-breadcrumb__item">
 					<NuxtLink class="c-breadcrumb__link" to="/works/">制作実績</NuxtLink>
 			</li>
-			<li class="c-breadcrumb__item">川越氷菓くもとゆき</li>
+			<li class="c-breadcrumb__item">{{ data.title }}</li>
 		</ul>
 </template>
