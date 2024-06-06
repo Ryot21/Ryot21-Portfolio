2dca0b4e229cdc6c33ba3fc003e2ad00531c11c6
Ryota Maeda <61216064+Ryot21@users.noreply.github.com>
2024-06-06 23:15:29 +0900

    制作実績 > ページネーション > 戻る・進むボタン

--- Ryot21-Portfolio/components/parts/Pagination.vue
+++ Ryot21-Portfolio/components/parts/Pagination.vue
@@ -15,16 +15,26 @@
 		if( page == current) return 'current';
 		return 'link';
 	}
+	console.log()
 </script>
 
 <template>
 	<!-- ページネーション -->
 	<div class="c-pagination">
 		<ul class="c-pagination__lists">
+			<!-- 戻る -->
+			<li v-if="current > 1">
+				<NuxtLink class="c-pagination__left s-M -center" :to="getPath(current - 1)">&laquo; Prev</NuxtLink>
+			</li>
+			<!-- 全ページ表示 -->
 			<li v-for="page in numPages" :key="page" class="c-pagination__item">
 				<NuxtLink class="c-pagination__link s-ML -center" :class="getClass(page, current)" :to="getPath(page)">
 					{{ page }}
 				</NuxtLink>
+			</li>
+			<!-- 進む -->
+			<li v-if="current < numPages">
+				<NuxtLink class="c-pagination__right s-M -center" :to="getPath(current + 1)">Next &raquo;</NuxtLink>
 			</li>
 		</ul>
 	</div>
